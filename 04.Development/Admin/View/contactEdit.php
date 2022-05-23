987e2b236659ce8dc007e371bdefcf1035dd7d23
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-05-23 14:43:03 +0630

    update contact edit

--- 04.Development/Admin/View/contactEdit.php
+++ 04.Development/Admin/View/contactEdit.php
@@ -8,12 +8,58 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="../resource/css/commonAdmin.css">
+    <link rel="stylesheet" href="../resource/css/contactEdit.css">
 </head>
 
 <body>
-    <div class="container">
-        <img src="../resource/image/logo.png" alt="">
+    <div class="container nav_box">
+        <img src="../resource/image/logo.png" alt="" class="logo" />
+        <hr />
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <span class="nav_details mt-4">
+                    <img src="../resource/image/home icon.png" alt="" class="nav_img" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Dashboard</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/author-icon-21.jpg" alt="" class="nav_img circle" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Author Info</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/book1.png" alt="" class="nav_img circle" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Book Info</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/order.png" alt="" class="nav_img" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Order Info</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/person icon.png" alt="" class="nav_img" />
+                    <a class="nav-link link_details" aria-current="page" href="#">User Info</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/person icon.png" alt="" class="nav_img" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Admin Settings</a>
+                </span>
+            </li>
+            <li class="nav-item">
+                <span class="nav_details mt-3">
+                    <img src="../resource/image/community.png" alt="" class="nav_img circle" />
+                    <a class="nav-link link_details" aria-current="page" href="#">Community</a>
+                </span>
+            </li>
+        </ul>
     </div>
 </body>
 
