0809bb03eec20e8d6381687aebc3a82d7d9e7048
khinhninkjy97@gmail.com <khinhninkjy97@gmail.com>
2022-05-25 01:14:03 +0630

    For Delivery Service

---
+++ 04.Development/Customer/View/deliveryService.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Delivery Service</title>
+    <link rel="stylesheet" href="../resource/css/deliveryService.css">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- notosan myanmar font link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Myanmar&display=swap" rel="stylesheet">
+</head>
+
+<body class="container-fluid container ">
+    <!-- <h4>ရေတာရှည်</h4> -->
+    <div >
+        <div class=" menubar_top ">
+            <a href="" class="logo mt-3 me-3"><img src="../resource/image/logo.png" alt=""></a>
+            <!-- <nav class="navbar"> -->
+            <ul class="navList ">
+                <li class="navItem"><a href="">မူလစာမျက်နှာ</a></li>
+                <li class="navItem"><a href="">စာအုပ်များ</a></li>
+                <li class="navItem"><a href="">စာရေးဆရာများ</a></li>
+                <li class="navItem"><a href="">အကောင့်သို့ဝင်ရန်</a></li>
+            </ul>
+            <div>
+                <a href=""><img src="../resource/image/useracc.jpg" alt="" class="useracc "></a>
+                <a href=""><span class="me-5">အကောင့်အမည်</span></a>
+                <a href="">
+                    <img src="../resource/image/cart.png" alt="" class="cart">
+                </a>
+                <span class="position-absolute  translate-middle badge bg-danger">2</span>
+
+            </div>
+        </div>
+
+        <div class="menubar_bottom">
+            <div class="menubarIcon">
+                <ion-icon name="menu" class="menubarIcon h-100 w-100"></ion-icon>
+            </div>
+            <!-- <div class="dropdown"> -->
+
+            <div class="dropdown">
+                <a class="btn  dropdown-toggle h-100" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown">
+                    ရှာဖွေရန်
+                </a>
+
+                <ul class="dropdown-menu " aria-labelledby="dropdownMenuLink">
+                    <li><a class="dropdown-item dpd" href="#">AUTHOR</a></li>
+                    <li><a class="dropdown-item dpd" href="#">BOOK</a></li>
+
+                </ul>
+            </div>
+
+            <!-- search -->
+            <div class="search">
+                <input type="text" name="" class="searchBox"
+                    placeholder="သင်ဘာကိုရှာဖွေချင်ပါသလဲ? ဒီကနေ ဝင်ရှာလို့ရပါတယ်">
+            </div>
+
+            <button class="figured  ">
+                <ion-icon name="search"></ion-icon>
+            </button>
+        </div>
+
+        <!-- body -->
+        <div class="deliList">
+            <div class="deliHeader mt-5">
+                အိမ်အရောက်ပို့ဆောင်ပေးသောနေရာများ
+            </div>
+
+            <div class="deliBody">
+                <!-- YangonDivision -->
+                <div class=" division yangon ">ရန်ကုန်တိုင်းဒေသကြီး</div>
+                <div class="yangonTownList">
+                    <ul class="yagonTown">
+                        <li>ဗိုလ်တထောင်</li>
+                        <li>ဒဂုံဆိပ်ကမ်း</li>
+                        <li>ဒေါပုံ</li>
+                        <li>အရှေ့ဒဂုံ</li>
+                        <li>မင်္ဂလာတောင်ညွှန့်</li>
+                        <li>မြောက်ဒဂုံ</li>
+                        <li>မြောက်ဥက္ကလာပ</li>
+                        <li>ပုဇွန်တောင်</li>
+                        <li>တောင်ဒဂုံ</li>
+                        <li>တောင်ဥက္ကလာပ</li>
+                        <li>တာမွေ</li>
+                        <li>သာကေတ</li>
+                        <li>သင်္ကန်းကျွန်း</li>
+                        <li>ရန်ကင်း</li>
+
+                        <li>အလုံ</li>
+                        <li>ဗဟန်း</li>
+                        <li>ဒဂုံ</li>
+                        <li>လှိုင်</li>
+                        <li>ကမာရွတ်</li>
+                        <li>ကျောက်တံတား</li>
+                        <li>ကြည့်မြင်တိုင်</li>
+                        <li>လမ်းမတော်</li>
+                        <li>လသာ</li>
+                        <li>မရမ်းကုန်း</li>
+                        <li>ပန်းဘဲတန်း</li>
+                        <li>စမ်းချောင်း</li>
+
+                        <li>ကိုကိုးကျွန်း</li>
+                        <li>ဒလ</li>
+                        <li>ကော့မှူး</li>
+                        <li>ခရမ်း</li>
+                        <li>ကွမ်းခြံကုန်း</li>
+                        <li>ကျောက်တန်း</li>
+                        <li>ဆိပ်ကြီးခနောင်တို</li>
+                        <li>သန်လျင်</li>
+                        <li>သုံးခွ</li>
+                        <li>တွံတေး</li>
+
+                        <li>လှိုင်သာယာ</li>
+                        <li>လှည်းကူး</li>
+                        <li>မှော်ဘီ</li>
+                        <li>ထန်းတပင်</li>
+                        <li>အင်းစိန်</li>
+                        <li>မင်္ဂလာဒုံ</li>
+                        <li>ရွှေပြည်သာ</li>
+                        <li>တိုက်ကြီး</li>
+                    </ul>
+                </div>
+                <!-- <hr class=" line lineYgn"> -->
+
+
+            </div>
+        </div>
+
+        <!-- FOOTER -->
+        <div class="footer ">
+            <div class="">@YadanarThike 2022</div>
+            <div class="copyright">&#169;
+                YadanarThike Company. All Rights Reserved.</div>
+        </div>
+</body>
+
+</html>
