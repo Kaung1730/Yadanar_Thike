b0c1dd97f26d6ea815d7a8db087235a5ab2f0013
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-05-25 13:44:43 +0630

    update

--- 04.Development/Admin/View/contact.php
+++ 04.Development/Admin/View/contact.php
@@ -10,6 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../resource/css/commonAdmin.css">
     <link rel="stylesheet" href="../resource/css/contactEdit.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <script src="../resource/js/jquery3.6.0.js"></script>
     <script src="../resource/js/contact.js"></script>
 </head>
@@ -23,7 +24,25 @@
                 </div>
                 <div class="col-10">
                     <div class="title_bar">
-                        
+                        <span>
+                            <span class="title mt-1 mx-3 fs-4">Community</span>
+                            <br />
+                            <span class="title_list ms-3" id="adminCl">Community</span>
+                            <span class="title_list">></span>
+                            <span class="title_list">Review & Rating</span>
+                        </span>
+                        <span class="box-search title-list ms-5 mb-3">
+                            <input class="form-control me-2 search-box mt-2" type="search" placeholder="Search" aria-label="Search">
+                        </span>
+                        <span class="title-list mt-3 ms-5">
+                            <i class="bi bi-chat-left-text-fill ms-5 fs-4"></i>
+                            <i class="bi bi-cart4 ms-5 fs-4 fw-bold"></i>
+                        </span>
+                        <span class="title-list mt-2 ms-5">
+                            <img src="../resource/image/person icon.png" alt="" class="nav_img ms-2">
+                            <br/>
+                            <span>Admin</span>
+                        </span>
                     </div>
                     <div class="contact-box mt-4">
                         <div class="head_text fs-3 pt-3 fw-normal">Website Info</div>
--- 04.Development/Admin/View/serviceList.php
+++ 04.Development/Admin/View/serviceList.php
@@ -26,12 +26,7 @@
                 </div>
                 <div class="col-10">
                     <div class="title_bar">
-                        <div class="title mt-1 mx-3 fs-4">Admin Settings</div>
-                        <span class="title_list ms-3" id="adminCl">Admin</span>
-                        <span class="title_list">></span>
-                        <span class="title_list" id="adSet">admin settings</span>
-                        <span class="title_list">></span>
-                        <span class="title_list">Privacy & Policy</span>
+                        
                     </div>
                     <div class="contact-box mt-4">
                         <div class="btn mt-4">
