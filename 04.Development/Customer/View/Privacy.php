494761de79ca38c66be0dff81eb47e11286de7d0
MSI\Hikari <thanthteinlin87@gmail.com>
2022-05-27 15:59:14 +0630

    thl

---
+++ 04.Development/Customer/View/Privacy.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Privacy</title>
+        <link rel="stylesheet" href="Privacy.css">
+        <link rel="stylesheet" href="commonUser.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <img class="top" src="top.png" alt="">
+        <div class="body">
+        <div class="smalltop">Privacy & Policy</div>
+        <div class="title">Personal Data & Cookie Policy</div>
+        
+        <div class="main">
+        <div class="left">
+            <div class="leftfact1">  1.  ကိုယ်ရေးကိုယ်တာအချက်အလက် နှင့်ဆိုင်သော</div>
+            <div class="leftfact2">  2.  ဝဘ်ဆိုဒ်ကို ဝင်ရောက်ကြည့်ရှုခွင့်၊ဒေတာကို ပြုပြင်ပြီး ဖျက်ပစ်ရန်နှင့် ဒေတာလုပ်ဆောင်ခြင်းကို ကန့်ကွက်ရန်</div>
+            <div class="leftfact3">  3.  စာရေးသူများအတွက် ကိုယ်ရေးကိုယ်တာ ကာကွယ်ရေး</div>
+            <div class="leftfact4">  4.  စာဖတ်သူများနှင့် ဖောက်သည်များအတွက် ကိုယ်ရေးကိုယ်တာ ကာကွယ်ရေး</div>
+        </div>
+        <div class="right">
+            <div class="rightfact1">Yadanar Thike သည် စာရွက်စာတမ်းများ သို့မဟုတ် ဝန်ဆောင်မှုများတောင်းဆိုမှုနှင့်ပတ်သက်၍ အသုံးပြုသူများထံမှ ဆိုက်မှတစ်ဆင့် ကိုယ်ရေးကိုယ်တာအချက်အလက်များကို စုဆောင်းနိုင်ပါသည်။</div>
+            <div class="rightfact2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+            <div class="rightfact3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+            <div class="rightfact4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div>
+        </div>
+        </div>
+        <div class="smallbot"> </div>
+        <img class="bot" src="Bottom.png" alt="">
+        
+    </div>
+    </body>
