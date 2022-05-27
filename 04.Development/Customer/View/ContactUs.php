a0fc67627cc05765867b8f472d5dcb9e9f429aa9
MSI\Hikari <thanthteinlin87@gmail.com>
2022-05-27 16:30:57 +0630

    pngs

--- 04.Development/Customer/View/ContactUs.php
+++ 04.Development/Customer/View/ContactUs.php
@@ -5,15 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Contact Us</title>
-        <link rel="stylesheet" href="ContactUs.css">
-        <link rel="stylesheet" href="commonUser.css">
+        <link rel="stylesheet" href="../resource/css/ContactUs.css">
+        <link rel="stylesheet" href="../resource/css/commonUser.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         
     </head>
     <body>
         <div class="body">
-        <img class="contact" src="contact.png" alt="">
+        <img class="contact" src="../resource/image/contact.png" alt="">
             <div class="main">
             
             <div class="left">
@@ -47,9 +47,9 @@
             <div class="gmail">info@yadanarthike.com, www.yadanarthike.com</div>
             <div class="follow">Follow us</div>
             <div class="icons">
-                <img class="icon" src="fb.png" alt="">
-                <img class="icon" src="ins.png" alt="">
-                <img class="icon" src="twi.png" alt="">
+                <img class="icon" src="../resource/image/fb.png" alt="">
+                <img class="icon" src="../resource/image/ins.png" alt="">
+                <img class="icon" src="../resource/image/twi.png" alt="">
             </div>
             </div>
             
--- 04.Development/Customer/View/GuideofWeb.php
+++ 04.Development/Customer/View/GuideofWeb.php
@@ -5,15 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>GuideofWeb</title>
-        <link rel="stylesheet" href="GuideofWeb.css">
-        <link rel="stylesheet" href="/04.Development/Customer/resource/css/commonUser.css">
+        <link rel="stylesheet" href="../resource/css/GuideofWeb.css">
+        <link rel="stylesheet" href="../resource/css/commonUser.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </head>
     <body>
     <div class="body">
         <div class="mytop">
-            <img class="imgs" src="Top1.png" alt="">
+            <img class="imgs" src="../resource/image/GoWtop.png" alt="">
         </div>
         <div class="smalltop">
         သင်၏စာအုပ်များကိုအွန်လိုင်းမှမှာယူပုံအဆင့်ဆင့်
@@ -32,7 +32,7 @@
             <div class="des"><p class="facts">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
         </div>
         <div class="mytop">
-            <img class="imgs" src="Bot.png" alt="" >
+            <img class="imgs" src="../resource/image/Gowbot.png" alt="" >
         </div>
         <div class="smalltop">
         သင် သိလိုသည့်မေးခွန်းများရှိပါက အကူအညီအတွက်  + 959 123 456 789 ကိုဖုန်းခေါ်ဆိုခြင်းဖြင့် ကျွန်ုပ်တို့ကိုဆက်သွယ်ပါ။
