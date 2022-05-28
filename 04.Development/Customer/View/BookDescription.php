34891ccd24b9feb3c803e6439b4f9c1a3ede8fe4
Kaung Kaung <kaungkaung1730@gmail.com>
2022-05-28 23:27:51 +0630

    Nav bar and footer update

--- 04.Development/Customer/View/BookDescription.php
+++ 04.Development/Customer/View/BookDescription.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="../resource/css/orderHistory.css">
     <link rel="stylesheet" href="../resource/css/nav.css">
     <link rel="stylesheet" href="../resource/css/order.css">
+    <link rel="stylesheet" href="../resource/css/footer.css ">
     <!--Bootstrap CSS & JavaScript-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -29,8 +30,10 @@
     <!--Nav-->
     <div class="nav-bar"></div>
     <!-- Start of Book Description -->
-    <div class="container py-5 mt-5">
-        <div class="upper-part">
+    <div class="container-fluid px-0  mt-5">
+        <!--Start of the book des-->
+            <div class="container">
+            <div class="upper-part">
             <div class="row">
                 <div class="col-lg-4 col-12  col-md-4 col-xl-4 ps-5 ps-lg-0 ps-md-0">
                     <img src="../resource/image/Chonechar-Mha-300x300.png" class="ms-5 ms-lg-0 ms-md-0 py-4 py-lg-0 py-md-0"/>
@@ -190,11 +193,11 @@
             </div>
         </div>
     </div>
-
-    <div class="container-fluid bg-warning">
-        <div class="footer"></div>
-    </div>
-    
+</div>
+        <!---End of the book des-->
+        <!--footer-->
+        <div class="footer d-flex py-3 px-2 text-center mt-4"></div>
+</div>
 </body>
 
 </html>
