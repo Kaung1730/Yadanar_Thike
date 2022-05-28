ae2e38cd13dddcc64fce7e537a87f7bb95422230
khinhninkjy97@gmail.com <khinhninkjy97@gmail.com>
2022-05-28 23:55:43 +0630

    edit

--- 04.Development/Customer/View/HomePage.php
+++ 04.Development/Customer/View/HomePage.php
@@ -29,12 +29,30 @@
     <!-- ionic icon -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
+<!--Customize CSS-->
+<link rel="stylesheet" href="../resource/css/bookDescription.css">
+    <link rel="stylesheet" href="../resource/css/commonUser.css">
+    <link rel="stylesheet" href="../resource/css/userSetting.css">
+    <link rel="stylesheet" href="../resource/css/cartPop.css">
+    <link rel="stylesheet" href="../resource/css/orderHistory.css">
+    <link rel="stylesheet" href="../resource/css/nav.css">
+    <link rel="stylesheet" href="../resource/css/order.css">
+    <link rel="stylesheet" href="../resource/css/footer.css ">
+    <!--Bootstrap CSS & JavaScript-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!--Bootstrap Icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <!--Slick JS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css">
+    <!--Customize JS-->
+    <script src="../resource/js/Cslik.js" defer></script>
+    <script src="../resource/js/nav.js"></script>
 </head>
 
 <body>
-    <?php require "./nav.php"; ?>
-
+<div class="nav-bar"></div>
     <div class="container ">
         <!-- HeaderSlider -->
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
--- 04.Development/Customer/View/shopList.php
+++ 04.Development/Customer/View/shopList.php
@@ -15,13 +15,32 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Myanmar&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="../resource/image/logo.png">
-
+<!--Customize CSS-->
+<link rel="stylesheet" href="../resource/css/bookDescription.css">
+    <link rel="stylesheet" href="../resource/css/commonUser.css">
+    <link rel="stylesheet" href="../resource/css/userSetting.css">
+    <link rel="stylesheet" href="../resource/css/cartPop.css">
+    <link rel="stylesheet" href="../resource/css/orderHistory.css">
+    <link rel="stylesheet" href="../resource/css/nav.css">
+    <link rel="stylesheet" href="../resource/css/order.css">
+    <link rel="stylesheet" href="../resource/css/footer.css ">
+    <!--Bootstrap CSS & JavaScript-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!--Bootstrap Icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <!--Slick JS-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css">
+    <!--Customize JS-->
+    <script src="../resource/js/Cslik.js" defer></script>
+    <script src="../resource/js/jquery3.6.0.js"></script>
+    <script src="../resource/js/nav.js"></script>
 </head>
 
 <body>
-    <?php require "./nav.php"; ?>
+<div class="nav-bar"></div>
     <div class="container ">
-        <div class="nav-bar"></div>
 
         <div class="shopList">
             <div class="shopListHeader">
