14952fd7b11bb7ffb3b56271c107cbc6ab854afc
khinhninkjy97@gmail.com <khinhninkjy97@gmail.com>
2022-05-29 00:38:17 +0630

    edit

--- 04.Development/Customer/View/HomePage.php
+++ 04.Development/Customer/View/HomePage.php
@@ -5,6 +5,8 @@
     <title>Home Page</title>
     <link rel="stylesheet" href="../resource/css/HomePage.css">
     <script src="../resource/js/jquery3.6.0.js"></script>
+    <script src="../resource/js/nav.js"></script>
+
     <link rel="stylesheet" href="../resource/css/commonUser.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -47,7 +49,7 @@
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css"> -->
     <!--Customize JS-->
-    <script src="../resource/js/nav.js"></script>
+   
 </head>
 
 <body>
