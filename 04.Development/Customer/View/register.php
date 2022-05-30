4048343b455ac734cf90327b0a0348cbf68ab067
Kaung Kaung <kaungkaung1730@gmail.com>
2022-05-31 00:16:29 +0630

    login and register color change

--- 04.Development/Customer/View/register.php
+++ 04.Development/Customer/View/register.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="../resource/image/logo.png">
     <!--Customize CSS-->
     <link rel="stylesheet" href="../resource/css/commonUser.css">
-    <link rel="stylesheet" href="../resource/css/login.css">
+    <link rel="stylesheet" href="../resource/css/register.css">
     <!--Bootstrap CSS & JavaScript-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -27,12 +27,12 @@
             </div>
             <div class="col-lg-7 p-4 p-md-3 p-lg-0 p-xl-0">
                 <form action="../Controller/registerController.php" class="" method="POST">
-                    <div class="row my-4">
+                    <div class="row mt-4">
                             <div class="col">
-                                <h3 class="text-center register-title">အ​ကောင့်ဖွင့်ရန်</h3>
+                                <h2 class="text-center text-white fw-normal">အ​ကောင့်ဖွင့်ရန်</h2>
                             </div>
                         </div>
-                        <div class="row my-3 justify-content-center">
+                        <div class="row mb-3 mt-2 justify-content-center">
                             <div class="col-lg-10">
                                 <input type="text" name="username" required placeholder="User Name (English Only)" class="form-control ps-4 pt-2"/>
                             </div>
@@ -57,7 +57,7 @@
                             <input type="number" name="phone" placeholder="Phone Number" class="form-control ps-4 pt-2"/>
                             </div>
                         </div>
-                        <div class="row mt-4 mb-3 justify-content-center">
+                        <div class="row my-3 justify-content-center">
                             <div class="col-lg-10 d-flex justify-content-between">
                                 <input type="submit" value="အ​ကောင့်ဖွင့်ရန်" class="btn btn-dark px-4 pt-2 text-center register-btn"/>
                                 <a href="./login.php" class="btn btn-light px-4 pt-2 text-center login-btn">အ​ကောင့်ဝင်ရန်</a>
