5a3cdbacfc249c9c818eb0c09b163437a1a677a8
Kaung Kaung <kaungkaung1730@gmail.com>
2022-06-04 00:52:35 +0630

    Category finished

---
+++ 04.Development/Customer/Controller/bookDespController.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "../Model/DBConnection.php";
+
+$id = $_GET['book_id'];
+
+//Call DB Connection
+$db =  new DBConnect();
+$dbconnect = $db->connect();
+
+$sql = $dbconnect->prepare("
+            SELECT * FROM book_m
+            WHERE book_id = :id;
+    ");
+
+$sql->bindValue(":id", $id);
+
+$sql->execute();
+
+$result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+require "../View/bookDescription.php";
