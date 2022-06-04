d1b1aa0a19e9cad7aad377d522777723e10378eb
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-06-04 17:18:31 +0630

    complete author backend but without other funtion eg. pagination and others;

--- 04.Development/Admin/Controller/authorDeleteController.php
+++ 04.Development/Admin/Controller/authorDeleteController.php
@@ -2,4 +2,15 @@
 
 require_once "../Model/dbConnection.php";
 
+$id = $_GET['id'];
 // call db connection
+$db = new DBConnect();
+$dbconnect = $db -> connect();
+$sql = $dbconnect -> prepare(
+    "UPDATE author SET del_flg=1 WHERE id = :id"
+);
+
+$sql->bindValue(":id",$id);
+$sql->execute();
+
+header("Location: ../View/authorList.php");
