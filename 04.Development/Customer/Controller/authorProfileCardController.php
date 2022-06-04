78647275f8094718bfa5246c62b758a288f49b5f
khinhninkjy97@gmail.com <khinhninkjy97@gmail.com>
2022-06-04 21:59:49 +0630

    author profile 's author books update

---
+++ 04.Development/Customer/Controller/authorProfileCardController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require_once "../Model/dbConnection.php";
+// $id = $_GET['id'];
+//call dbConnection
+$db2 = new DBConnect();
+$dbconnect = $db2->connect();
+
+
+
+$sql = $dbconnect->prepare("SELECT * FROM book_m 
+LEFT JOIN author
+ON book_m.author_id = author.id  ; 
+");
+// $sql->bindValue(":id", $id);
+//go to run
+$sql->execute();
+
+
+$result = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+// echo "<pre>";
+// print_r($result);
+
+//connect with View
+// require "../View/authorProfile.php";
