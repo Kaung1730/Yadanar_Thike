13dffed150b4b31577a397f21772360ad205ff96
Kaung Kaung <kaungkaung1730@gmail.com>
2022-06-04 23:27:15 +0630

    error update

--- 04.Development/Customer/Controller/bookListController.php
+++ 04.Development/Customer/Controller/bookListController.php
@@ -1,40 +1,6 @@
 <?php
 require_once "../Model/DBConnection.php";
 
-<<<<<<< HEAD
-//Call DB Connection
-//call db connection
-$db = new DBConnect();
-$dbConnect = $db->connect();
-$category_name =  $dbConnect->prepare("
- SELECT * FROM category
- ");
-$yokePya = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 1
- ");
-$novel = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 2
- ");
-$language = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 3
- ");
-$health = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 4
- ");
-$politic = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 5
- ");
-$poem = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 6
- ");
-$success = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 7
- ");
-$biography = $dbConnect->prepare("
- SELECT * FROM book_m where category_id = 8
- ");
-$other = $dbConnect->prepare("
-=======
  //Call DB Connection
  //call db connection
  $db = new DBConnect();
@@ -67,7 +33,6 @@ $other = $dbConnect->prepare("
  SELECT * FROM book_m where category_id = 8
  ");
  $other = $dbConnect -> prepare("
->>>>>>> origin/main
  SELECT * FROM book_m where category_id = 9
  ");
 $category_name->execute();
@@ -80,18 +45,6 @@ $poem->execute();
 $success->execute();
 $biography->execute();
 $other->execute();
-<<<<<<< HEAD
-$result = $category_name->fetchAll(PDO::FETCH_ASSOC);
-$yokePyaResult = $yokePya->fetchAll(PDO::FETCH_ASSOC);
-$novelResult = $novel->fetchAll(PDO::FETCH_ASSOC);
-$languageResult = $language->fetchAll(PDO::FETCH_ASSOC);
-$healthResult = $health->fetchAll(PDO::FETCH_ASSOC);
-$politicResult = $politic->fetchAll(PDO::FETCH_ASSOC);
-$poemResult = $poem->fetchAll(PDO::FETCH_ASSOC);
-$successResult = $success->fetchAll(PDO::FETCH_ASSOC);
-$biographyResult = $biography->fetchAll(PDO::FETCH_ASSOC);
-$otherResult = $other->fetchAll(PDO::FETCH_ASSOC);
-=======
  $result = $category_name->fetchAll(PDO::FETCH_ASSOC);
  $yokePyaResult = $yokePya->fetchAll(PDO::FETCH_ASSOC);
  $novelResult = $novel->fetchAll(PDO::FETCH_ASSOC);
@@ -102,4 +55,3 @@ $otherResult = $other->fetchAll(PDO::FETCH_ASSOC);
  $successResult = $success->fetchAll(PDO::FETCH_ASSOC);
  $biographyResult = $biography->fetchAll(PDO::FETCH_ASSOC);
  $otherResult = $other->fetchAll(PDO::FETCH_ASSOC);
->>>>>>> origin/main
