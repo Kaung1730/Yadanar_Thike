61c740f8c5404096664a7e718f7cc64a3dc5b8fa
Kaung Kaung <kaungkaung1730@gmail.com>
2022-06-08 01:01:20 +0630

    Contact us Page UI fix (Complete)

--- 04.Development/Customer/View/ContactUs.php
+++ 04.Development/Customer/View/ContactUs.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <div class="body">
+    <!-- <div class="body">
         <img class="contact" src="../resource/image/contact.png" alt=""/>
         <div class="main">
 
@@ -55,6 +55,42 @@
                 </div>
             </div>
 
+        </div>
+    </div> -->
+    <div class="container d-flex justify-content-center align-items-center mb-0 mb-md-0 mb-lg-4">
+        <div class="left mt-lg-5 mt-md-3 mt-3 pb-2 pb-lg-2 pb-md-2 pe-3 pe-md-4 pe-lg-2 mb-4 mb-md-3 mb-lg-0">
+            <div class="title lh-lg ps-5 mt-3">Contact Us</div>
+
+            <div class="facts lh-lg pb-3 ps-5">သင်ဖြည့်စွက်ပြောလိုသည့်အကြောင်းကို ကျွန်ုပ်တို့အား ထပ်မံပြောပြပေးပါ မကြာမီ သင့်ထံ အကြောင်းကြားပေးပါမည်။</div>
+            <div class="input pb-2">
+                <label class="label lh-lg">User ၏ အမည်</label>
+                <input class="inputbox i1 lh-lg" type="text" placeholder="အမည်"/>
+            </div>
+            <div class="input pb-2">
+                <label class="label lh-lg">ဆက်သွယ်ရမည့်ဖုန်းနံပါတ်</label>
+                <input class="inputbox i2 lh-lg" type="text" placeholder="+959 123 456 789"/>
+            </div>
+            <div class="input">
+                <label class="label lh-lg">ဖြည့်စွက်ပြောလိုသည့်အကြောင်းအရာ</label>
+                <textarea name="" class="i3 lh-lg" cols="10" rows="2"></textarea>
+            </div>
+            <input type="submit" class="btn send-btn float-end me-5 px-5 rounded-pill" value="ပေးပို့ရန်"/>
+        </div>
+
+        <div class="right mt-5 py-4 d-none d-md-none d-lg-block">
+        <div class="righttitle ms-3">Contact အချက်အလက်</div>
+                <div class="rightfact rightfontSize  pt-2">၁၂၃ ရတနာလမ်း,</div>
+                <div class="rightfact rightfontSize pt-3 fs-6 fw-bold">>>>>>>> </div>
+                <div class="rightfact rightfontSize pt-3"> ရန်ကုန်</div>
+                <div class="ps-3 pt-2 rightfontSize"> +၉၅၉ ၁၂၃ ၄၅၆ ၇၈၉ ကို ဆက်သွယ်ပါ၊</div>
+                <div class="ps-3 pt-2 rightfontSize">နေ့စဉ် မနက် ၉း၀၀ မှ ညနေ ၆း၀၀ အထိ ဖွင့်ပါသည်။</div>
+                <div class="gmail">info@yadanarthike.com, www.yadanarthike.com</div>
+                <div class="follow fs-4">Follow us</div>
+                <div class="icons">
+                    <img class="icon" src="../resource/image/fb.png" alt=""/>
+                    <img class="icon" src="../resource/image/ins.png" alt=""/>
+                    <img class="icon" src="../resource/image/twi.png" alt=""/>
+                </div>
         </div>
     </div>
 </body>
