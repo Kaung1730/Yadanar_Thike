9c25dd18f8fb0e5c9ff18700dd4f84166ff759aa
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-06-16 00:11:51 +0630

    fix design

--- 04.Development/Admin/View/commonLayout/dashboardLayout.php
+++ 04.Development/Admin/View/commonLayout/dashboardLayout.php
@@ -2,28 +2,28 @@
     <div class="total_user">
         <div class="d-flex justify-content-around box">
             <img src="../resource/img/person icon.png" alt="" class="person_icon pt-3">
-            <span class="pt-3 fs-2 text-success fw-bold">5000</span>
+            <span class="pt-3 fs-2 text-success fw-bold font-num">5000</span>
         </div>
         <p class="user_text">Total Users</p>
     </div>
     <div class="total_viewers">
         <div class="d-flex justify-content-around box">
             <img src="../resource/img/person icon.png" alt="" class="person_icon pt-3">
-            <span class="pt-3 fs-2 text-danger fw-bold">2000</span>
+            <span class="pt-3 fs-2 text-danger fw-bold font-num">2000</span>
         </div>
         <p class="viewer_text">Total Viewers</p>
     </div>
     <div class="income">
         <div class="d-flex justify-content-around box">
             <img src="../resource/img/person icon.png" alt="" class="person_icon pt-3">
-            <span class="pt-3 fs-2 text-primary fw-bold">1.5k</span>
+            <span class="pt-3 fs-2 text-primary fw-bold font-num">1.5k</span>
         </div>
         <p class="income_text">Income</p>
     </div>
     <div class="order">
         <div class="d-flex justify-content-around box">
             <img src="../resource/img/person icon.png" alt="" class="person_icon pt-3">
-            <span class="pt-3 fs-2 text-info fw-bold">300</span>
+            <span class="pt-3 fs-2 text-info fw-bold font-num">300</span>
         </div>
         <p class="order_text">Order</p>
     </div>
