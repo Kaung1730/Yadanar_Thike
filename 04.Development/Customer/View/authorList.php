c617f5874ee81489c7b0e9f483e33058ef8dbd99
khinhninkjy97@gmail.com <khinhninkjy97@gmail.com>
2022-06-16 00:30:37 +0630

    author list responsive completed

--- 04.Development/Customer/View/authorList.php
+++ 04.Development/Customer/View/authorList.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
 <head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Author List</title>
     <link rel="stylesheet" href="../resource/css/authorList.css">
     <link rel="shortcut icon" href="../resource/image/logo.png">
