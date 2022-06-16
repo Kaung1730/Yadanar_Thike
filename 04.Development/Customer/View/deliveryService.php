1cdfbd88f50ea95ef11d4c6b93ad40a455d50488
DESKTOP-DT2AHDC\A <pyaephyohein22august@gmail.com>
2022-06-17 01:46:16 +0630

    update responsive by PPH

--- 04.Development/Customer/View/deliveryService.php
+++ 04.Development/Customer/View/deliveryService.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
 <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Delivery Service</title>
     <link rel="shortcut icon" href="../resource/image/logo.png">
     <link rel="stylesheet" href="../resource/css/deliveryService.css">
@@ -58,7 +61,7 @@
                 <?php
                 require "../Controller/stateController.php";
                 for ($i = 1; $i <= 15; $i++) {
-                    $id =$i;
+                    $id = $i;
                     require "../Controller/townshipController.php";
                     echo "<div class ='division yangon'>" . $result[0]['state_name'] . "</div>";
                     echo "<div class='yangonTownList'>";
@@ -67,6 +70,7 @@
                         echo "<li>" . $result[$j]['township_name'] . "</li>";
                     }
                     echo "</div>";
+                    echo "</hr>";
                 }
                 ?>
             </div>
