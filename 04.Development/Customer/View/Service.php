ae27afd882d23f91a25fad7611cc21381b797f8b
DESKTOP-DT2AHDC\A <pyaephyohein22august@gmail.com>
2022-06-17 01:21:04 +0630

    update responsive by PPH

--- 04.Development/Customer/View/Service.php
+++ 04.Development/Customer/View/Service.php
@@ -1,73 +1,77 @@
 <!DOCTYPE html>
 <html lang="my-MM">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Service</title>
-        <link rel="stylesheet" href="../resource/css/service.css">
-        <link rel="stylesheet" href="../resource/css/commonUser.css">
-        <script src="jquery-3.6.0.min.js"></script>
-    </head>
-    <body>
-    <div class="body">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Service</title>
+    <link rel="stylesheet" href="../resource/css/service.css">
+    <link rel="stylesheet" href="../resource/css/commonUser.css">
+    <script src="jquery-3.6.0.min.js"></script>
+    <link rel="shortcut icon" href="../resource/image/logo.png">
+    <!--Customize CSS-->
+    <link rel="stylesheet" href="../resource/css/bookDescription.css">
+    <link rel="stylesheet" href="../resource/css/commonUser.css">
+    <link rel="stylesheet" href="../resource/css/userSetting.css">
+    <link rel="stylesheet" href="../resource/css/cartPop.css">
+    <link rel="stylesheet" href="../resource/css/orderHistory.css">
+    <link rel="stylesheet" href="../resource/css/nav.css">
+    <link rel="stylesheet" href="../resource/css/order.css">
+    <link rel="stylesheet" href="../resource/css/footer.css ">
+    <!--Bootstrap CSS & JavaScript-->
+    <!--Bootstrap Icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+        <!--Customize JS-->
+        <script src="../resource/js/cSlik.js" defer></script>
+    <script src="../resource/js/jquery3.6.0.js"></script>
+    <script src="../resource/js/nav.js"></script>
+</head>
+
+<body>
+    <div class="nav-bar"></div>
+    <div class="setting"></div>
+    <div class="cart"></div>
+    <div class="order"></div>
+    <div class="orderHistory"></div>
+    <div class="container service">
         <div class="main">
             <div class="header">
                 <p class="title">မင်္ဂလာပါ… ဝန်ဆောင်မှုများစွာဖြင့် Yadanar Thikeမှ ကြိုဆိုပါသည်
                 </p>
             </div>
             <div class="logos">
-            <img class="logo1" src="../resource/image/Servicelogo1.png" alt="">
-            <img class="logo2" src="../resource/image/Servicelogo2.png" alt="">
+                <img class="logo1" src="../resource/image/Servicelogo1.png" alt="">
+                <img class="logo2" src="../resource/image/Servicelogo2.png" alt="">
             </div>
-        <div class="service">
-            <div class="service-a">
-                <?php   
-                require("../Controller/serviceController.php");
-                    foreach($result as $key => $value){
+            <div class="service">
+                <div class="service-a">
+                    <?php
+                    require("../Controller/serviceController.php");
+                    foreach ($result as $key => $value) {
                         echo "<div class='tab'>";
                         echo "<div class='up'>";
-                        echo "<div class='png'>"; 
-                        echo  "<img src='../resource/image/".$value['icon']."' alt='' class='img'>";
+                        echo "<div class='png'>";
+                        echo  "<img src='../resource/image/" . $value['icon'] . "' alt='' class='img'>";
                         echo "</div>";
-                        echo "<p class='detail a'>". $value['question']. "</p>";
+                        echo "<p class='detail a'>" . $value['question'] . "</p>";
                         echo "</div>";
                         echo "<div class='down'>";
-                        echo "<p class='back'>".$value['answer']."</p>";
+                        echo "<p class='back'>" . $value['answer'] . "</p>";
                         echo "</div>";
                         echo "</div>";
                     }
-
-                ?>
-                <!-- <div class="tab">
-                    <div class="up">
-                    <div class="png"> <img src="../resource/image/Servicebookmark.png" alt="" class="img"></div>
-                    <p class="detail a">စာမှတ်ကဒ် အခမဲ့</p></div>
-                    <div class="down">
-                    <p class="back">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
+                    ?>
                 </div>
-                <div class="tab">
-                    <div class="up">
-                    <div class="png"> <img src="../resource/image/Servicedelifee.png" alt="" class="img"></div>
-                    <p class="detail b">ပို့ခနှုန်းထား</p></div>
-                    <div class="down">
-                    <p class="back">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div>
-                <div class="tab">
-                    <div class="up">
-                    <div class="png"> <img src="../resource/image/Servicedelivery.png" alt="" class="img"></div>
-                    <p class="detail c">ပြည်တွင်းပို့ဆောင်မှုစနစ်</p></div>
-                    <div class="down">
-                    <p class="back">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>
-                </div> -->
+            </div>
+            <div class="footer">
             </div>
         </div>
-        <div class="footer">
-        </div>
-        </div>
     </div>
-    </body>
+</body>
+
 </html>
