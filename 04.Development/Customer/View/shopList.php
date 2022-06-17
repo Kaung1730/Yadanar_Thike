72b6fb39f961329508913c79129c2cab53bad82c
DESKTOP-DT2AHDC\A <pyaephyohein22august@gmail.com>
2022-06-18 01:30:58 +0630

    update

--- 04.Development/Customer/View/shopList.php
+++ 04.Development/Customer/View/shopList.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
 <head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Shop List</title>
     <link rel="stylesheet" href="../resource/css/shopList.css">
     <script src="../resource/js/jquery3.6.0.js"></script>
