cb430594133571acc95368e6baf78e3b1ae8d44e
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-06-21 12:20:00 +0630

    update dashBoard

--- 04.Development/Admin/View/adminLogout.php
+++ 04.Development/Admin/View/adminLogout.php
@@ -3,5 +3,5 @@ session_start();
  unset($_SESSION['status']);
  unset($_SESSION['admin_name']);
  unset($_SESSION['admin_email']);
- header("location: ./dashboard.php");
+ header("location: ./dashBoard.php");
 ?>
