037600afc0968ea56a69ea491c100cf53a33459a
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2022-06-24 01:26:04 +0630

    update image post

--- 04.Development/Admin/Controller/homeScreenController.php
+++ 04.Development/Admin/Controller/homeScreenController.php
@@ -8,37 +8,26 @@ if (isset($_POST)) {
     $icon = $_FILES['icon']['name'];
     $location2 = $_FILES['icon']['tmp_name'];
     $del_flg = 0;
+    $id = $_POST['id'];
 
-    if (move_uploaded_file($location1, "../resource/upload_img/" . $logo || move_uploaded_file($location2, "../resource/upload_img/" . $icon))) {
+    if (move_uploaded_file($location1, "../resource/image/" . $logo || move_uploaded_file($location2, "../resource/image/" . $icon))) {
 
         $db = new DBConnect();
         $dbconnect = $db->connect();
         $sql = $dbconnect->prepare(
-            "INSERT INTO home_master
-            (
-                logo,
-                icon,
-                name,
-                del_flg,
-                created_date,
-                created_by
-            )
-            VALUES
-            (
-                :logo,
-                :icon,
-                :name,
-                :del_flg,
-                :created_date,
-                :created_by
-            );"
+            "UPDATE home_master SET
+                logo = :logo,
+                icon = :icon,
+                name = :name,
+                updated_date = :updated_date,
+                updated_by = :updated_by"
         );
         $sql->bindValue(":logo", $logo);
         $sql->bindValue(":icon", $icon);
         $sql->bindValue(":name", $shopName);
-        $sql->bindValue(":del_flg", $del_flg);
-        $sql->bindValue(":created_date", date("d/m/Y"));
-        $sql->bindValue(":created_by", "myat kaung khant");
+        // $sql->bindValue(":id",$id);
+        $sql->bindValue(":updated_date", date("d/m/Y"));
+        $sql->bindValue(":updated_by", "myat kaung khant");
         $sql->execute();
 
         header("location: ../View/homeScreen.php");
