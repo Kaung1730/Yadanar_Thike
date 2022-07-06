0b23790640884d7e95ca95aa610c01141f55c015
Kaung Kaung <kaungkaung1730@gmail.com>
2022-07-07 00:53:33 +0630

    query error update

--- 04.Development/Customer/Controller/stateController.php
+++ 04.Development/Customer/Controller/stateController.php
@@ -1,12 +1,12 @@
 <?php
-    require_once"../Model/dbConnection.php";
+    require_once"../Model/DBConnection.php";
     //call dbConnection
     $db1 = new DBConnect();
     $dbconnect1 = $db1->connect();
 
-    $sql1 = $dbconnect1 -> prepare("SELECT * FROM state WHRE del_flg = :del_flg");
+    $sql1 = $dbconnect1 -> prepare("SELECT * FROM state WHERE del_flg = :del_flg");
     //go to run
-    $sql -> bindValue(":del_flg",0);
+    $sql1 -> bindValue(":del_flg",0);
     $sql1 -> execute();
     $result1 = $sql1 -> fetchAll(PDO::FETCH_ASSOC);
     // echo "<pre>";
